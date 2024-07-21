d4dabfbe6d9d7ab3834355d1dd9de5edfbf1e87b
roscoemiras <114201361+roscoemiras@users.noreply.github.com>
2024-07-21 15:44:56 +0200

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -6,7 +6,20 @@ System::System(double G, double delta_time, vector<Body> bodies) {
     this->bodies = bodies;
 }
 void System::move() {
-    // ...
+    for (Body body : this->bodies) {
+        body.update_acceleration(bodies, this->delta_time, this->G);
+    }
+    for (Body body : this->bodies) {
+        body.update_coordinate_next(this->delta_time);
+    }
+    for (Body body : this->bodies) {
+        body.update_acceleration_next(bodies, this->delta_time, this->G);
+    }
+    for (Body body : this->bodies) {
+        body.update_velocity_next(this->delta_time);
+        body.move();
+    }
+
 }
 vector<Body> System::get_bodies() {
     return this->bodies;
