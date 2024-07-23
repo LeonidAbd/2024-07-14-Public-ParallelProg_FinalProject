37ebb3a3a4586d86c76cd534eb545e7cb470aa8e
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-07-23 18:30:14 +0200

    fix: ide code formatting

--- system.cpp
+++ system.cpp
@@ -1,6 +1,7 @@
 #include "system.h"
 
-System::System() {
+System::System()
+{
     this->G = 1;
     this->delta_time = 1.0 / 32.0;
     this->bodies = {};
@@ -8,26 +9,33 @@ System::System() {
     this->time = 0;
 }
 
-System::System(double G, double delta_time, vector<Body> bodies) {
+System::System(double G, double delta_time, vector<Body> bodies)
+{
     this->G = G;
     this->delta_time = delta_time;
     this->bodies = bodies;
     this->is_acceleration_initialized = false;
     this->time = 0;
 }
-void System::move() {
+void System::move()
+{
     // if the acceleration is not initialized yet, we must do it here
-    if (!this->is_acceleration_initialized) {
-        for (int body_id = 0; body_id < this->bodies.size(); body_id++) {
+    if (!this->is_acceleration_initialized)
+    {
+        for (int body_id = 0; body_id < this->bodies.size(); body_id++)
+        {
             this->bodies[body_id].update_acceleration(this->bodies, this->delta_time, this->G);
             this->bodies[body_id].update_coordinate_next(this->delta_time);
         }
         this->is_acceleration_initialized = true;
-    } else {
+    }
+    else
+    {
         for (int body_id = 0; body_id < this->bodies.size(); body_id++)
             this->bodies[body_id].update_coordinate_next(this->delta_time);
     }
-    for (int body_id = 0; body_id < this->bodies.size(); body_id++) {
+    for (int body_id = 0; body_id < this->bodies.size(); body_id++)
+    {
         // we have to make a new cycle, because
         // the acceleration_next needs all the bodies with updated coordinate_next
         this->bodies[body_id].update_acceleration_next(this->bodies, this->delta_time, this->G);
@@ -37,10 +45,12 @@ void System::move() {
     this->time += this->delta_time;
 }
 
-vector<Body> System::get_bodies() {
+vector<Body> System::get_bodies()
+{
     return this->bodies;
 }
 
-double System::get_time() {
+double System::get_time()
+{
     return this->time;
 }
